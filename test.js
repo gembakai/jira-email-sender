f806964a1e6badff284b536b2a99a57593462f20
Gembakai <gembakai@Gembakai-Mac-Book.local>
2024-08-24 13:47:01 -0600

    Initial commit

---
+++ test.js
@@ -0,0 +1,10 @@
+const sendEmail = require('./sendEmail');
+
+// Datos de ejemplo de una incidencia
+const issueData = {
+    summary: 'Problema en el servidor',
+    description: 'El servidor está experimentando un alto consumo de CPU.',
+};
+
+// Llamar a la función para enviar el correo
+sendEmail(issueData);
