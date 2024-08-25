794858e84ee0e6801f34af6226ddce1903e6fbf2
Gembakai <admin@gembakai.com>
2024-08-25 14:20:21 -0600

    Cuerpo del correo en HTML

--- sendEmail.mjs
+++ sendEmail.mjs
@@ -50,7 +50,9 @@ const sendEmail = async (issueData) => {
     try {
         // Extraer los valores específicos que necesitas
         const issueKey = issueData.issue.key || 'No disponible';
-        const customField10038 = issueData.issue.fields.customfield_10038 || 'No disponible';
+        const customField10038 = issueData.issue.fields.customfield_10038 || 'No disponible'; // Institución
+        const customField10039 = issueData.issue.fields.customfield_10039 || 'No disponible';  // Número de procedimiento
+        const customField10042 = issueData.issue.fields.customfield_10042 || 'No disponible'; // Vencimiento de respuesta
 
         // Extraer los valores de la lista múltiple (customfield_10057)
         const customField10057Array = issueData.issue.fields.customfield_10057 || [];
@@ -68,19 +70,89 @@ const sendEmail = async (issueData) => {
             })
         );
 
-        // Formatear el contenido del correo
+        // Formatear el contenido del correo en HTML
         const emailContent = `
-        Detalles de la incidencia:
-        - Clave de la incidencia: ${issueKey}
-        - Custom Field 10038: ${customField10038}
-        - Departamentos: ${customField10057Values}
-        `;
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Nueva Oportunidad - Grupo DIM</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    background-color: #f4f4f4;
+                    color: #333;
+                }
+                .container {
+                    width: 80%;
+                    margin: 20px auto;
+                    background-color: #fff;
+                    padding: 20px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                }
+                .header {
+                    text-align: left;
+                    margin-bottom: 30px;
+                }
+                .header img {
+                    max-width: 200px;
+                    height: auto;
+                }
+                .divider {
+                    border-top: 5px solid #8d8d8d; /* Azul */
+                    margin: 20px 0;
+                }
+                .content {
+                    margin-top: 10px;
+                    font-size: 16px;
+                    line-height: 1.4;
+                    color: #1a1a1a;
+                }
+                .content p {
+                    margin-bottom: 4px;
+                }
+                .footer {
+                    margin-top: 20px;
+                    text-align: center;
+                    font-size: 12px;
+                    color: #777;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">
+                    <img src="https://static.wixstatic.com/media/49738c_f42431ee087d4c618729e3f1480ccc73~mv2.jpg" alt="Grupo DIM Logo">
+                </div>
+                <div class="divider"></div>
+                <div class="content">
+                    <h2>NUEVA OPORTUNIDAD</h2>
+                    <p><strong>Institucion: </strong>${customField10038}</p>
+                    <p><strong>Número de procedimiento: </strong>${customField10039}</p>
+                    <p><strong>Dirigido a:</strong> ${customField10057Values}</p>
+                    <p><strong>Fecha de respuesta a:</strong> ${customField10042}</p>
+                    <h3>Observaciones:</h3>
+                    <p></p>
+                    <div class="divider"></div>
+                    <p>Sírvase encontrar en el correo adjunto los elementos necesarios para tramitar esta solicitud. Si tiene alguna duda puede contactarse con Paola Chaves Mora 2228-8191.</p>
+                </div>
+                <div class="footer">
+                    <p>www.grupodim.net | San José, Costa Rica</p>
+                    <p>Antes de imprimir piensa en tu responsabilidad con el medio ambiente.</p>
+                    <p>Before printing think about your environmental responsibility</p>
+                </div>
+            </div>
+        </body>
+        </html>`;
 
         let info = await transporter.sendMail({
             from: '"Gembakai" <admin@gembakai.com>', // Remitente
             to: 'wballestero@gembakai.com, will.ballestero@outlook.com', // Destinatarios
-            subject: `Nueva Incidencia: ${issueKey}`, // Asunto del correo
-            text: emailContent, // Contenido del correo con los datos específicos
+            subject: `Nueva Oportunidad: ${customField10038}`, // Asunto del correo
+            html: emailContent, // Contenido del correo en HTML
             attachments: attachmentFiles // Adjuntar los archivos descargados
         });
 
