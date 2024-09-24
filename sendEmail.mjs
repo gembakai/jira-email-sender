c323661b487e53bc43319dfa0fbce0f263b0a8c1
Gembakai <admin@gembakai.com>
2024-09-24 10:47:14 -0600

    cambios

--- sendEmail.mjs
+++ sendEmail.mjs
@@ -6,7 +6,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-// Necesitas estas dos líneas para manejar __dirname en ES Modules
+// Necesitas estas dos líneas para manejar __dirname en ES Moduless
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
